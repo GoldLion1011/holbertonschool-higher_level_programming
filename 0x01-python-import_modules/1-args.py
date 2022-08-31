50d8d3010e0fcff684a469b4b39adecd6d811753
GoldLion1011 <yogirose79@gmail.com>
2022-08-31 11:29:42 -0700

    last attempt task 1

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+from sys import argv
+
 if __name__ == "__main__":
-    import sys
-    num = len(sys.argv) - 1
-    if num == 0:
+    nArgs = len(argv)
+    if nArgs == 1:
         print("0 arguments.")
-    elif num == 1:
+    elif nArgs == 2:
         print("1 argument.")
     else:
-        print("{} arguments:".format(num))
+        print("{} arguments:".format(nArgs - 1))
 
-    for i in range(num):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    for i in range(1, nArgs):
+        print("{}: {}".format(i, argv[i]))
