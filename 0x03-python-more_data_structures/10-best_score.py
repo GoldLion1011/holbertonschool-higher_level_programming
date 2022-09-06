e1ceb19ba3257c2a7c3d822e625a3462bac4332e
GoldLion1011 <yogirose79@gmail.com>
2022-09-06 12:05:03 -0700

    task 10 corrected again

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max value = max(a_dictionary.values())
-    return max_value
+    if not a_dictionary:
+        return None
+    return max(a_dictionary)
