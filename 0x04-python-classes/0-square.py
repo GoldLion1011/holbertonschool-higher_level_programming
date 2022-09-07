239968627ae7cd96e43719268ebeedddbdfeddfd
GoldLion1011 <yogirose79@gmail.com>
2022-09-07 11:12:35 -0700

    tasks 0 and 1

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Class that defines an empty square"""
+    def __init__(self, size):
+        """Initializes the square and sets its size"""
+        self.__size = size
