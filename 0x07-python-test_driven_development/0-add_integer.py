afb3f31101d62333af25fa1cf162aee9263c1f95
GoldLion1011 <yogirose79@gmail.com>
2022-09-12 09:19:48 -0700

    task and test 0 corrections

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,14 +10,14 @@ def add_integer(a, b=98):
     """Returns the sum of two integers a and b,
        returns an error if a and b are not ints or floats"""
 
-    if type(a) is not int or float:
+    if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
-    if type(b) is not int or float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
 
     if type(a) is float:
-        type(a) = a
+        a = int
     if type(b) is float:
-        type(b) = b
+        b = int
 
     return a + b
