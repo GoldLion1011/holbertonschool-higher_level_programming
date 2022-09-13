ef84262a8818d3dd43114dc0b91a7b73d48211e9
GoldLion1011 <yogirose79@gmail.com>
2022-09-13 10:38:10 -0700

    task 0 pep8 and syntax

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -6,37 +6,38 @@ Defines a rectangle class
 
 class Rectangle:
     """ Class that defines a rectangle """
-    def __init__(self, width= 0, height= 0):
+
+    def __init__(self, width=0, height=0):
         """ Initializes a new rectangle.
 
         Arguments:
                   width (int): width of new rectangle
                   height (int): height of new rectangle
         """
-    self.width = width
-    self.height = height
+        self.width = width
+        self.height = height
 
     @property
-    def width(self)
-    """ Gets and sets the width of the rectangle """
+    def width(self):
+        """ Gets and sets the width of the rectangle """
         return self__.width
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if not type(value) is not int:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
-        seld.__width = value
+        self.__width = value
 
     @property
     def height(self):
         """ Gets and sets the height of the rectangle """
         return self__.height
 
-    @heightsetter
+    @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
