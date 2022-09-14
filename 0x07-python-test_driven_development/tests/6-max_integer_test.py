2bf3ec3efaf565e0d8b9781936d9c5f556f90127
GoldLion1011 <yogirose79@gmail.com>
2022-09-13 18:09:04 -0700

    total redo

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,17 +5,21 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-class TestMaxInteger(unittest.TestCase):
-    def test_max_integer(unittest.TestCase):
-        self.assertEqual(max_integer("string"), 't')
-        self.assertEqual(max_integer([], None)
-        self.assertEqual(max_integer([4], 4)
-        self.assertEqual(max_integer([1, 2, 3, 4], 1)
-        self.assertEqual(max_integer([1, 2, 4, 3], 3)
-        self.assertEqual(max_integer([1.0, 2], 2)
-        self.assertEqual(max_integer([-1, -2, -3, -4], -1)
-        self.assertRaises(TypeError, max_integer, 4)
-        self.assertRaises(TypeError, max_integer, ["string", 4, 5, 1]
 
-if __name__== '__main__':
+class TestMaxInteger(unittest.TestCase):
+    def test_max_integer(self):
+        self.assertEqual(max_integer([3, 2, 5]), 5)
+        self.assertEqual(max_integer([3, 6, 5]), 6)
+        self.assertEqual(max_integer([9, 2, 5]), 9)
+        self.assertEqual(max_integer([2]), 2)
+        self.assertEqual(max_integer([-4, -3, -5]), -3)
+        self.assertEqual(max_integer([]), None)
+        self.assertNotEqual(max_integer([3, 2, 5]), 8)
+        self.assertNotEqual(max_integer([1, 2, 3]), 9)
+        self.assertNotIn(max_integer([7, 4, 8]), [9, 2, 5])
+        self.assertIsInstance(max_integer([6, 3, 7]), int)
+        self.assertTrue(max_integer([6, 3, 7]), int)
+        self.assertFalse(max_integer([0, 0, 0]), False)
+
+if __name__ == '__main__':
     unittest.main()
