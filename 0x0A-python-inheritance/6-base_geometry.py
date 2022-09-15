99cebb262e9fb67f4113f9cfe90bb118642a6f81
GoldLion1011 <yogirose79@gmail.com>
2022-09-15 13:08:17 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module: 6-base_geometry"""
+
+
+class BaseGeometry():
+    """ BaseGeometry class """
+    def area(self):
+        raise Exception("area() is not implemented")
