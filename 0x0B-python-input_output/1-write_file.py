e34d10f20f2daac742f7f4eeeb9c929137e6a90a
GoldLion1011 <yogirose79@gmail.com>
2022-09-19 08:32:15 -0700

    tasks 0-2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-""" """
+""" Module 1-write_file
+    writes a string to a text file (UTF8) and
+    returns the number of characters written """
 
 
 def write_file(filename="", text=""):
-    """ """
+    """ writes a string to a text file (UTF8) and
+        returns the number of characters written"""
+
     with open(filename, 'w+') as f:
         return f.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-""" """
+""" Module 2-append_write
+    appends a string at the end of a text file """
 
 
 def append_write(filename="", text=""):
-    """ """
+    """ appends a string at the end of a text file (UTF8)
+        and returns the number of characters added """
+
     with open(filename, 'a+') as f:
         return f.write(text)
