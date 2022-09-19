a8bf2cf0dbc85974b608dd77ed551e8b03276eff
GoldLion1011 <yogirose79@gmail.com>
2022-09-19 13:49:10 -0700

    task 9 corrections

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,13 +3,17 @@
     Creates a class Student """
 
 
-class Student():
-    """ defining student attributes """
+class Student:
+    """ defining Student attributes """
 
-    def __init__(first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
+        """ initializes the Student instance """
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        return vars(self)
+        """ retrieves dictionary representation of Student instance """
+
+        return self.__dict__
