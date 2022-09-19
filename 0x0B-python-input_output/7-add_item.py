e2a6ecc54a9ae442044aac93e8fe791598088a57
GoldLion1011 <yogirose79@gmail.com>
2022-09-19 12:14:11 -0700

    task 7 v.3

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,23 +4,19 @@
     and then save them to a file """
 
 
-import json
 import sys
-import os.path
-
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-my_list = []
-my_file = 'add_item.json'
+try:
+    my_file = load('add_item.json')
 
-if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
-    my_list = load_from_json_file(my_file)
+except(TypeError, FileNotFoundError):
+    my_file = []
 
-if len(sys.argv) > 1:
-    for elem in sys.argv[1:]:
-        my_list.append(elem)
+for i in sys.argv[1:]:
+    my_file.append(i)
 
-save_to_json_file(my_list, my_file)
+save(my_list, 'add_item.json)
