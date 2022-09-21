d70d7b28503de2bf7131ed340a1d3145d98e7913
GoldLion1011 <yogirose79@gmail.com>
2022-09-21 11:39:05 -0700

    task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,29 @@ class Square(Rectangle):
     def __str__(self):
         """ sets string value of Square """
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.size}'
+
+    def update(self, *args, **kwargs):
+        """ assigns value to an unknown number of arguments """
+
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
+
+        for key, value in kwargs.items():
+            if key == "id":
+                self.id = value
+            if key == "width":
+                self.width = value
+            if key == "height":
+                self.height = value
+            if key == "x":
+                self.x = value
+            if key == "y":
+                self.y = value
