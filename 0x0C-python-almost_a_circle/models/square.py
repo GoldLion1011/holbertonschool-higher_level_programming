a335317338d5cfd72b19a138937dc796564c05ec
GoldLion1011 <yogirose79@gmail.com>
2022-09-21 10:48:46 -0700

    square time v2

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ initializes the Square """
 
-        super().__init__(size, size, id, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ sets string value of Square """
