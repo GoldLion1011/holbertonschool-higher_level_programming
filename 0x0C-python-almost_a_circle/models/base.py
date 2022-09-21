c2d0106f3b6938d4e5d0ffa5a7f8825b5d7a60d1
GoldLion1011 <yogirose79@gmail.com>
2022-09-21 14:16:10 -0700

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
     defines Base class for other classes in this project """
 
 
+import json
+
+
 class Base:
     """ class with private attribute
         __nb_objects """
@@ -19,3 +22,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ returns JSON str representation of list_dictionaries """
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries)
