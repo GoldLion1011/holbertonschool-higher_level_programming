e3313ca5b694d80f993136043eb986248b7820a8
GoldLion1011 <yogirose79@gmail.com>
2022-09-21 09:19:17 -0700

    task 9 corrected again

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -104,35 +104,25 @@ class Rectangle(Base):
     def update(self, *args, **kwargs):
         """ assigns value to an unknown number of arguments """
 
-        if len(args) == 1:
+        if len(args) >= 1:
             self.id = args[0]
-        if len(args) == 2:
-            self.id = args[0]
+        if len(args) >= 2:
             self.width = args[1]
-        if len(args) == 3:
-            self.id = args[0]
-            self.width = args[1]
+        if len(args) >= 3:
             self.height = args[2]
-        if len(args) == 4:
-            self.id = args[0]
-            self.width = args[1]
-            self.height = args[2]
+        if len(args) >= 4:
             self.x = args[3]
-        if len(args) == 5:
-            self.id = args[0]
-            self.width = args[1]
-            self.height = args[2]
-            self.x = args[3]
+        if len(args) >= 5:
             self.y = args[4]
 
         for key, value in kwargs.items():
             if key == "id":
-                self.id = value['id']
+                self.id = value
             if key == "width":
-                self.width = value['width']
+                self.width = value
             if key == "height":
-                self.height = value['height']
+                self.height = value
             if key == "x":
-                self.x = value['x']
+                self.x = value
             if key == "y":
-                self.y = value['y]
+                self.y = value
