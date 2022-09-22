327e3021fe8d837be9ed68d32279c0fb7c192fe6
GoldLion1011 <yogirose79@gmail.com>
2022-09-22 15:45:24 -0700

    even moar rectangle tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,7 +3,9 @@
     Test cases for Rectangle class """
 
 
+from logging import exception
 import unittest
+from unittest.case import _AssertRaisesContext
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
@@ -11,18 +13,52 @@ from models.square import Square
 
 class TestRectangle(unittest.TestCase):
     """ Test class for Rectangle """
-    def test_docstrings(self):
-        """ check module and func documentation"""
-        self.assertTrue(len(Rectangle.__init__.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.width.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.height.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.x.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.y.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.area.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.display.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.__str__.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.update.__doc__) >= 1)
-        self.assertTrue(len(Rectangle.to_dictionary.__doc__) >= 1)
+    def test_rect_id(self):
+        """ check Rectangle for id """
+        r1 = Rectangle(1, 2)
+        self.assertEqual(r1.id, 1)
+        r2 = Rectangle(2, 3)
+        self.assertEqual(r2.id, 2)
+        r3 = Rectangle(3, 4)
+        self.assertEqual(r3.id, 3)
+        r4 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r4.id, 12)
+        r5 = Rectangle(10, 2, 4, 5, 34)
+        self.assertEqual(r5.id, 34)
+        r6 = Rectangle(10, 2, 4, 5, -6)
+        self.assertEqual(r6.id, -6)
+        r7 = Rectangle(10, 2, 4, 5, 8)
+        self.assertEqual(r7.id, 8)
 
+    def test_rect_attrs(self):
+        """ check Rectangle for attribute values """
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        r2 = Rectangle(10, 2, 4, 5, 24)
+        self.assertEqual(r2.width, 10)
+        self.assertEqual(r2.height, 2)
+        self.assertEqual(r2.x, 4)
+        self.assertEqual(r2.y, 5)
+
+    def test_rect_args(self):
+        """ checks Rectangle for missing arguments """
+        r1 = Rectangle(3)
+        self.assertEqual("__init__() missing 1 required positional argument:
+                         'height'", str()x.exception))
+        s = ("__init__() missing 2 required positional arguments: 'width' and 'height'")
+        with self._assertRaises(TypeError) as x:
+            r2 = Rectangle
+        self.assertEqual(s, str(x.exception))
+
+    def test_rect_inherit(self):
+        """ checks Rectangle for inheritance """
+        r1 = Rectangle(9, 3)
+        self.assertTrue(isinstance(r1, Base))
+        self.assertTrue(issubclass(Rectangle, Base))
+        self.assertFalse(isinstance(Rectangle, Base))
+        
 if __name__ == '__main__':
     unittest.main()
