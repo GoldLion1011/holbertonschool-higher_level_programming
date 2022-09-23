8da32c558a08b0033cee69c357ea3b917dfbbb57
GoldLion1011 <yogirose79@gmail.com>
2022-09-23 13:03:33 -0700

    teststesttests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -41,5 +41,35 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r1_dictionary, test_dict)
         self.assertEqual(type(r1_dictionary), dict)
 
+    def test_area(self):
+        """ checks area """
+        r1 = Rectangle(3, 2, 0, 0, 99)
+        self.assertEqual(r1.area(), 6)
+
+    def test_display(self):
+        """ """
+        pass
+
+    def test__str__(self):
+        """ checks for str return """
+        r1 = Rectangle(3, 2, 0, 0, 99)
+        self.assertEqual(str(r1), '[Rectangle] (99) 0/0 - 3/2')
+
+    def test_update(self, *args, **kwargs):
+        """ checks for dict updates """
+        r1 = Rectangle(3, 2, 0, 0, 99)
+        r1.update(100, 4, 3, 1, 1)
+        self.assertEqual(r1.width, 4)
+        self.assertEqual(r1.height, 3)
+        self.assertEqual(r1.x, 1)
+        self.assertEqual(r1.y, 1)
+        self.assertEqual(r1.id, 100)
+        r1.update(width=5, height=4, x=2, y=2, id=101)
+        self.assertEqual(r1.width, 5)
+        self.assertEqual(r1.height, 4)
+        self.assertEqual(r1.x, 2)
+        self.assertEqual(r1.y, 2)
+        self.assertEqual(r1.id, 101)
+
 if __name__ == '__main__':
     unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -37,5 +37,24 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s1_dictionary, test_dict)
         self.assertEqual(type(s1_dictionary), dict)
 
+    def test__str__(self):
+        """ checks for str return """
+        s1 = Square(10, 2, 1, 99)
+        self.assertEqual(str(s1), '[Square] (99) 2/1 - 10')
+
+    def test_update(self, *args, **kwargs):
+        """ checks for dict updates """
+        s1 = Square(10, 2, 1, 99)
+        s1.update(100, 1, 10, 2)
+        self.assertEqual(s1.size, 1)
+        self.assertEqual(s1.x, 10)
+        self.assertEqual(s1.y, 2)
+        self.assertEqual(s1.id, 100)
+        s1.update(size=2, x=11, y=3, id=101)
+        self.assertEqual(s1.size, 2)
+        self.assertEqual(s1.x, 11)
+        self.assertEqual(s1.y, 3)
+        self.assertEqual(s1.id, 101)
+
 if __name__ == '__main__':
     unittest.main()
