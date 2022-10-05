6dc8cc89e70d3219bfa3ab863cf7cd631f870847
GoldLion1011 <yogirose79@gmail.com>
2022-10-05 12:31:21 -0700

    tasks 9-13

---
+++ 0x00-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists all records in the table second_table in db hbtn_0c_0
+SELECT score, name
+    FROM second_table
+    ORDER BY score DESC;
+    
---
+++ 0x00-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- Updates "Bob's" score to 10 in second_table
+UPDATE 'Bob' SET score = 10 WHERE score = 10;
