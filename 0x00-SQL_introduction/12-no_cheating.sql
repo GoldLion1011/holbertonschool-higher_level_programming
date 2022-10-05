d6c8eed0fb25e41c1c8d44853991a0c819e7bfeb
GoldLion1011 <yogirose79@gmail.com>
2022-10-05 12:51:18 -0700

    tasks 4, 9, 12, 13 corrected

--- 0x00-SQL_introduction/12-no_cheating.sql
+++ 0x00-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,4 @@
 -- Updates "Bob's" score to 10 in second_table
-UPDATE 'Bob' SET score = 10 WHERE score = 10;
+UPDATE second_table
+     SET score = 10 
+     WHERE name = 'Bob';
--- 0x00-SQL_introduction/13-change_class.sql
+++ 0x00-SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
 -- Removes all records with a score <= 5 in second_table (db hbtn_0c_0)
 DELETE FROM second_table
-    WHERE score >= 5;
+    WHERE score <= 5;
