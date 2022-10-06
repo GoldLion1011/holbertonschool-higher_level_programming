4feb0615540cfc56abd09502f008f10b40c19045
GoldLion1011 <yogirose79@gmail.com>
2022-10-06 12:02:44 -0700

    syntax ugh

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Lists all the cities of California found in db hbtn_0d_usa
-SELECT cities.id, cities.name FROM cities, states WHERE states.name = 'California' AND state.id = states.id ORDER BY cities.id
+SELECT cities.id, cities.name FROM cities, states WHERE states.name = 'California' AND state_id = states.id ORDER BY cities.id
--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,5 @@
 SELECT cities.id, cities.name, states.name
     FROM cities
     LEFT JOIN states
-    ON states_id = states.id
+    ON state_id = states.id
     ORDER BY cities.id ASC
-     
