010b86ed1c58a100b01c20247c0ad41186b8929e
GoldLion1011 <yogirose79@gmail.com>
2022-10-06 09:15:11 -0700

    files, README, tasks 0-4

---
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Creates db hbtn_0d_2 and user user_0d_2
+CREATE DATABASE hbtn_0d_2;
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
---
+++ 0x01-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- Create a new table id_not_null on server
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT NOT NULL DEFAULT(1),
+    name VARCHAR(256)
+);
