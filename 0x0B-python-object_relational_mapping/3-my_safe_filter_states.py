d44e64d749917e9c65a73f46cee36c0c63dbca6f
GoldLion1011 <yogirose79@gmail.com>
2022-10-19 14:11:23 -0700

    minor tweeks on 3 again

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,18 +8,19 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
+
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE BINARY name = %(states)s \
-                ORDER BY id;", {'state': sys.argv[4]})
+    state = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE LIKE BINARY %(name)s \
+                ORDER BY id ASC", {'name': sys.argv[4]})
     
     results = cur.fetchall()
 
-    for row in results:
-        print(row)
+    for record in results:
+        print(record)
 
     cur.close()
     db.close()
