de28435b06ee6b5e1927de8a516bfbee47aa92d8
GoldLion1011 <yogirose79@gmail.com>
2022-10-19 14:24:35 -0700

    sick of 6 too

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Class definition of Base """
 
-import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declaritive import declarative_base
 
@@ -11,5 +10,5 @@ Base = declarative_base()
 class State(Base):
     """ State class that inherits from Base"""
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False, auto_increment=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
