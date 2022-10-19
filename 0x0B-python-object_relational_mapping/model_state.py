e6a069dead793edbca68de740f29b21a5a06441e
GoldLion1011 <yogirose79@gmail.com>
2022-10-19 11:57:15 -0700

    import base model

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -3,7 +3,7 @@
 
 import sqlalchemy
 from sqlalchemy import Column, Integer, String
-from sqlalchemy import.ext.declaritive import declaritive_base
+from sqlalchemy.ext.declaritive import declaritive_base
 
 Base = declarative_base()
 
