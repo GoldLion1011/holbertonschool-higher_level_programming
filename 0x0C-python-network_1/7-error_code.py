342c5c2950580796dc414a4b891fbba3ca90b3b0
GoldLion1011 <yogirose79@gmail.com>
2022-11-01 15:08:33 -0700

    task 7 again

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -6,9 +6,10 @@ import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    req = requests.get(url)
-    if req.status_code >= 400:
-        print('Error code: {}'.format(req.status_code))
+    req = requests.get(sys.argv[1])
+    status = req.status_code
+    if status >= 400:
+        print('Error code: {}'.format(status))
     else:
         print(req.text)
+
