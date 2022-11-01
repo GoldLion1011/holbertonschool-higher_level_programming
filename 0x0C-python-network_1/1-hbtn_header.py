637ed1c1b2e2bb310c9e2c5472513b4e870ffc3e
GoldLion1011 <yogirose79@gmail.com>
2022-11-01 11:51:10 -0700

    typo???

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -2,12 +2,11 @@
 """" takes in a URL, sends a request to the URL
  and displays the value of the X-Request-Id variable
  found in the header of the response """
-import urlib
+from urllib import request
 import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    request = urlib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get('X-Request-Id'))
+    req = request.Request(sys.argv[1])
+    with request.urlopen(req) as response:
+        print(response.info()['X-Request-Id'])
