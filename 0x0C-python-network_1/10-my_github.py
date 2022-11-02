40dc42f173c0654fb7f4c38ccfb3e09810900b05
GoldLion1011 <yogirose79@gmail.com>
2022-11-02 10:04:56 -0700

    task 9/10, chmod 6 and 7

---
+++ 0x0C-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    username = sys.argv[1]
+    token = sys.argv[2]
+    req = requests.get(url, auth=HTTPBasicAuth(username, token)).json()
+    print(req.get('id'))
