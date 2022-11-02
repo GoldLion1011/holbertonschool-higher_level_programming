1ce40cfda587ab3b274746b52bfdda62047100c1
GoldLion1011 <yogirose79@gmail.com>
2022-11-02 09:56:45 -0700

    correction 7

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -7,8 +7,7 @@ import sys
 
 if __name__ == "__main__":
     req = requests.get(sys.argv[1])
-    status = req.status_code
-    if status >= 400:
-        print('Error code: {}'.format(status))
+    if req.status_code >= 400:
+        print('Error code: ', req.status_code)
     else:
         print(req.text)
